c8d02330fac0b84d3210912e4d8739cde3a04ffe
yaojungang <yaojungang@9bc8e58c-9903-d7e3-2926-6bdea2ce10d8>
2012-04-17 02:16:49 +0000

    bugfix

---
+++ cache/smarty/idc/2bd632aaacce5b4c996652c09d613dabce370dc0.file.index.tpl.php
@@ -0,0 +1,29 @@
+<?php /* Smarty version Smarty-3.0.8, created on 2012-04-13 21:10:45
+         compiled from "/home/y109/Develop/php/cidc/application/modules/idc/views/scripts/group/index.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:6841335184f8825d5d42552-34493465%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '2bd632aaacce5b4c996652c09d613dabce370dc0' => 
+    array (
+      0 => '/home/y109/Develop/php/cidc/application/modules/idc/views/scripts/group/index.tpl',
+      1 => 1318858672,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '6841335184f8825d5d42552-34493465',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+<script type="text/javascript" src="<?php echo $_smarty_tpl->getVariable('this')->value->baseUrl('/');?>
+js/idc/group.js"></script>
+<div class="mainContent">
+    <div class="pageTitle">
+        <?php echo $_smarty_tpl->getVariable('this')->value->title;?>
+
+    </div>
+    <div class="clear"></div>
+    <div id="MainPanel"></div>
+</div>
---
+++ cache/smarty/idc/dc079c61eb79fa652eb992854a123876d8acc7fa.file.index.tpl.php
@@ -0,0 +1,30 @@
+<?php /* Smarty version Smarty-3.0.8, created on 2012-04-13 21:09:55
+         compiled from "/home/y109/Develop/php/cidc/application/modules/idc/views/scripts/index/index.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:5382008124f8825a31ac5f0-82269815%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'dc079c61eb79fa652eb992854a123876d8acc7fa' => 
+    array (
+      0 => '/home/y109/Develop/php/cidc/application/modules/idc/views/scripts/index/index.tpl',
+      1 => 1319099014,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '5382008124f8825a31ac5f0-82269815',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+<script type="text/javascript" src="<?php echo $_smarty_tpl->getVariable('this')->value->baseUrl('/');?>
+js/idc/main.js"></script>
+<div class="mainContent">
+    <div class="pageTitle">
+        <?php echo $_smarty_tpl->getVariable('this')->value->title;?>
+
+    </div>
+    <div class="clear"></div>
+    <div id="MainPanel">
+    </div>
+</div>
